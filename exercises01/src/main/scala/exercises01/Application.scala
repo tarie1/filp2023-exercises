c997f6248d9bf1e6c8c1afa9e57eda6606eca786
Daria Zinkina <dzinkina@bk.ru>
2023-02-19 17:19:15 +0500

    Зинкина Дарья 1

--- exercises01/src/main/scala/exercises01/Application.scala
+++ exercises01/src/main/scala/exercises01/Application.scala
@@ -1,7 +1,7 @@
 package exercises01
 
 object Application extends App {
-  def hello(name: String): String = "Hi " + name
+  def hello(name: String): String = "Hello " + name
 
   println(
     hello("world")
--- exercises01/src/main/scala/exercises01/Vector.scala
+++ exercises01/src/main/scala/exercises01/Vector.scala
@@ -1,28 +1,49 @@
 package exercises01
 
 class Vector(val x: Double, val y: Double) {
-  def +(other: Vector): Vector = ???
+  def +(other: Vector): Vector = new Vector(x.+(other.x), y.+(other.y))
 
-  def -(other: Vector): Vector = ???
+  def -(other: Vector): Vector = new Vector(x - other.x, y - other.y)
 
-  def *(scalar: Double): Vector = ???
+  def *(scalar: Double): Vector = new Vector(x * scalar, y * scalar)
 
-  def unary_- : Vector = ???
+  def unary_- : Vector = new Vector(-x, -y)
 
-  def euclideanLength: Double = ???
+  def euclideanLength: Double = Math.sqrt(x * x + y * y)
 
-  def normalized: Vector = ???
+  def normalized: Vector = {
+    if (x != 0 && y != 0) {
+      new Vector(x / Math.sqrt(x * x + y * y), y / Math.sqrt(x * x + y * y))
+    } else {
+      if (x==0){
+        new Vector(0, y / Math.sqrt(x * x + y * y))
+      }
+      if (y == 0){
+        new Vector(x / Math.sqrt(x * x + y * y), 0)
+      }
+      new Vector(0, 0)
+    }
+  }
 
-  override def equals(other: Any): Boolean = ???
-
+  override def equals(other: Any): Boolean = {
+    this.hashCode() == other.hashCode()
+  }
   // Vector(x, y)
-  override def toString: String = ???
+  override def toString: String = new String(s"Vector($x, $y)")
 }
 
 object Vector {
-  def fromAngle(angle: Double, length: Double): Vector = ???
+  def fromAngle(angle: Double, length: Double): Vector = new Vector(length * Math.cos(angle), Math.sin(angle) * length)
 
-  def sum(list: List[Vector]): Vector = ???
+  def sum(list: List[Vector]): Vector = {
+    var x: Double = 0
+    var y: Double = 0
+    for (vector <- list){
+      x = x + vector.x
+      y = y + vector.y
+    }
+    new Vector(x, y)
+  }
 
-  def unapply(arg: Vector): Option[(Double, Double)] = ???
+  def unapply(arg: Vector): Option[(Double, Double)] = Option(arg.x, arg.y)
 }

9810a44f1622e7cf51e8daf6137a0b836dd0af0c
s.zaydelman <s.zaydelman@tinkoff.ru>
2023-03-02 15:38:47 +0500

    fixed run.sh

--- .github/run.sh
+++ .github/run.sh
@@ -1,36 +1,37 @@
 export PROJECT_NAME="exercises01"
 export SCALAFIX=""
+export BRANCH_NAME=${GITHUB_HEAD_REF##*/}
 
-if [ "$GITHUB_REF" == "exercises02" ]; then
+if [ $BRANCH_NAME == "exercises02" ]; then
   export PROJECT_NAME="exercises02"
-elif [ "$GITHUB_REF" == "exercises03" ]; then
+elif [ $BRANCH_NAME == "exercises03" ]; then
   export PROJECT_NAME="exercises03"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises04" ]; then
+elif [ $BRANCH_NAME == "exercises04" ]; then
   export PROJECT_NAME="exercises04"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises05" ]; then
+elif [ $BRANCH_NAME == "exercises05" ]; then
   export PROJECT_NAME="exercises05"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises06" ]; then
+elif [ $BRANCH_NAME == "exercises06" ]; then
   export PROJECT_NAME="exercises06"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises07" ]; then
+elif [ $BRANCH_NAME == "exercises07" ]; then
   export PROJECT_NAME="exercises07"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises08" ]; then
+elif [ $BRANCH_NAME == "exercises08" ]; then
   export PROJECT_NAME="exercises08"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises09" ]; then
+elif [ $BRANCH_NAME == "exercises09" ]; then
   export PROJECT_NAME="exercises09"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises10" ]; then
+elif [ $BRANCH_NAME == "exercises10" ]; then
   export PROJECT_NAME="exercises10"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises11" ]; then
+elif [ $BRANCH_NAME == "exercises11" ]; then
   export PROJECT_NAME="exercises11"
   export SCALAFIX="scalafix"
-elif [ "$GITHUB_REF" == "exercises12" ]; then
+elif [ $BRANCH_NAME == "exercises12" ]; then
   export PROJECT_NAME="exercises12"
   export SCALAFIX="scalafix"
 fi

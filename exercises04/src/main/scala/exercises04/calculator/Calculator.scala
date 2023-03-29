ee02415c3628e6d2628f81ccd3b5ee2880aa7a92
Daria Zinkina <dzinkina@bk.ru>
2023-03-29 12:20:11 +0500

    добавила map и flatmap в result и сократила код

--- exercises04/src/main/scala/exercises04/calculator/Calculator.scala
+++ exercises04/src/main/scala/exercises04/calculator/Calculator.scala
@@ -7,60 +7,26 @@ class Calculator[T: Integral] {
   def isZero(t: T): Boolean =
     t == implicitly[Integral[T]].zero
 
-  def calculate(expr: Expr[T]): Result[T] =
+  def calculate(expr: Expr[T]): Result[T] = {
     expr match {
-      case Mul(left, right) =>
-        calculate(left) match {
-          case Success(val1) =>
-            calculate(right) match {
-              case DivisionByZero => DivisionByZero
-              case Success(val2)  => Success(val1 * val2)
-            }
-        }
-      case Plus(left, right) =>
-        calculate(left) match {
-          case Success(val1) =>
-            calculate(right) match {
-              case Success(val2)  => Success(val1 + val2)
-              case DivisionByZero => DivisionByZero
-            }
-        }
-      case Minus(left, right) =>
-        calculate(left) match {
-          case Success(val1) =>
-            calculate(right) match {
-              case DivisionByZero => DivisionByZero
-              case Success(val2)  => Success(val1 - val2)
-            }
-        }
+      case Mul(left, right)   => calculate(left).flatMap(val1 => calculate(right).map(val2 => val1 * val2))
+      case Plus(left, right)  => calculate(left).flatMap(val1 => calculate(right).map(val2 => val1 + val2))
+      case Minus(left, right) => calculate(left).flatMap(val1 => calculate(right).map(val2 => val1 - val2))
       case Div(left, right) =>
-        calculate(right) match {
-          case Success(val2) =>
-            isZero(val2) match {
-              case true => DivisionByZero
-              case false =>
-                calculate(left) match {
-                  case Success(val1) => Success(val1 / val2)
-                }
-            }
-        }
+        calculate(right).flatMap(val2 =>
+          isZero(val2) match {
+            case true  => DivisionByZero
+            case false => calculate(left).map(val1 => val1 / val2)
+          }
+        )
       case Val(v) => Success(v)
       case If(iff, cond, left, right) =>
-        calculate(cond) match {
-          case Success(value) =>
-            iff(value) match {
-              case true =>
-                calculate(left) match {
-                  case DivisionByZero => DivisionByZero
-                  case Success(value) => Success(value)
-                }
-              case false =>
-                calculate(right) match {
-                  case DivisionByZero => DivisionByZero
-                  case Success(value) => Success(value)
-                }
-            }
-        }
-
+        calculate(cond).flatMap(value =>
+          iff(value) match {
+            case true  => calculate(left).map(value => value)
+            case false => calculate(right).map(value => value)
+          }
+        )
     }
+  }
 }
--- exercises04/src/main/scala/exercises04/calculator/Result.scala
+++ exercises04/src/main/scala/exercises04/calculator/Result.scala
@@ -1,5 +1,14 @@
 package exercises04.calculator
 
-sealed trait Result[+A]
-case class Success[+A](value: A) extends Result[A]
+sealed trait Result[+A] {
+  def map[B](f: A => B): Result[B] = this match {
+    case Success(a)     => Success(f(a))
+    case DivisionByZero => DivisionByZero
+  }
+  def flatMap[B](f: A => Result[B]): Result[B] = this match {
+    case DivisionByZero => DivisionByZero
+    case Success(value) => f(value)
+  }
+}
+case class Success[+A](value: A) extends Result[A] {}
 case object DivisionByZero       extends Result[Nothing]
